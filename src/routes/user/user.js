344c66515b678bf7225e2238b6f61ad1f095c010
icarobteles <eng.icaroteles@gmail.com>
2022-06-02 09:09:38 -0300

    feat(routes): :sparkles: create the routes to userRouter

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -1,3 +1,9 @@
 import { Router } from "express";
+import { readById, remove, update } from "../../controllers/user.js";
+import checkToken from "../../middlewares/auth.js";
 
 export const userRouter = Router();
+
+userRouter.get("/:id", checkToken, readById);
+userRouter.patch("/:id", checkToken, update);
+userRouter.delete("/:id", checkToken, remove);
